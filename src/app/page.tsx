da0ab19efbb9b001a69419e1186643df072b9f2d
Peerawat Poombua <peerawat.pm@gmail.com>
2025-01-17 03:20:10 +0700

    feat: show post in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { db } from "~/server/db";
+import { posts } from "~/server/db/schema";
+
 const mockURLs = [
   "https://kk5ufmcho3.ufs.sh/f/6J4TnvydzIe8E0pvaxNWUoTGI326A90Q5eZLJPzaYw7fBdxg",
   "https://kk5ufmcho3.ufs.sh/f/6J4TnvydzIe8vTaR9E1jEUsHtWaCQBomnYwquGSc0OMpe2gI",
@@ -7,10 +10,18 @@ const mockURLs = [
 
 const mockImages = mockURLs.map((url, index) => ({ id: index + 1, url }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const ps = await db.select().from(posts);
+  // const posts = await db.query.posts.findMany();
+
   return (
     <main>
       <div className="flex flex-wrap gap-3">
+        {ps.map((post) => (
+          <div key={`${post.id}`} className="text-black">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-1/4">
             <img className="w-full" src={image.url} alt="mock" />
